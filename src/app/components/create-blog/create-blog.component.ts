46cd62e503b233734c976f9cecca07de66a318f5
DiegoTalledoS18 <85322967+DiegoTalledoS18@users.noreply.github.com>
2023-12-30 12:13:31 -0500

    video

--- src/app/components/create-blog/create-blog.component.ts
+++ src/app/components/create-blog/create-blog.component.ts
@@ -9,6 +9,7 @@ export class CreateBlogComponent implements OnInit {
 
   constructor() { }
   htmlContent='';
+  firebaseVideoUrl = 'https://firebasestorage.googleapis.com/v0/b/agripure-678b4.appspot.com/o/Comp%202.mp4?alt=media&token=5d0a2c99-fdad-4d69-b3d0-59d297d5dee3';
   ngOnInit(): void {
   }
 
